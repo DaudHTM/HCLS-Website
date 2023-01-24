7c7f772b3daa357307db7592213bd04cdcfb5a09
DaudHTM <DaudHTM@gmail.com>
2023-01-23 23:09:25 -0500

    started frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import React, { Component } from 'react';
+
 import './App.css';
-import { useState } from 'react';
+import Nav from "./components/Nav";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 import Axios from "axios";
 
 function App() {
-  const [listOfUsers, setListOfUsers ] = useState([])
+  
   return (
-   <div> hello</div>
+    <BrowserRouter>
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+
+      </Routes>
+   
+   
+    </BrowserRouter>
+
+    
+
   );
 }
 
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      dasg
+    </div>
+  )
+}
+
+export default Dashboard
